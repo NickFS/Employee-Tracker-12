18d154f750069a648f26ca26bce314734f9a8d39
NickFS <nickolas.t.foster@gmail.com>
2020-10-27 23:07:16 -0600

    seeds add

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,36 @@
-DROP DATABASE IF EXISTS employeeDB;
 
-CREATE DATABASE employeeDB;
+DROP DATABASE IF EXISTS employees_DB;
 
-USE employeeDB;
+CREATE DATABASE employees_DB;
 
+USE employees_DB;
+
+----- Create Department table -----
 CREATE TABLE department (
-    id INT auto_increment PRIMARY KEY NOT NULL,
-    name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
+----- Create Role Table -----
 CREATE TABLE role (
-    id INT auto_increment PRIMARY KEY NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(9,2) NOT NULL,
-    department_id INT NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
+----- Create Employee Table -----
 CREATE TABLE employee (
-    id INT auto_increment PRIMARY KEY NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
+
